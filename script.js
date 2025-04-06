8917b44eb902538134a53d2ed053976b661b04af
medjz1 <MohamedElAzhar.Jaouadi@esprit.tn>
2025-04-06 22:58:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,57 +7,52 @@ document.addEventListener('DOMContentLoaded', () => {
     const memories = {
         mountain1: {
             title: "First Challenge",
-            content: "Remember when we...",
+            content: "Deciding to go through this together despite the distance and the journey lying ahead was a definitely a big step that i'm proud of",
             heart: "red-heart.png"
         },
         mountain2: {
             title: "Big Obstacle",
-            content: "That time we...",
-            heart: "red-heart.png"
-        },
-        mountain3: {
-            title: "Recent Triumph",
-            content: "I'm proud we...",
+            content: "I know that we have to wait but every second that passes makes me look forward to be with you more",
             heart: "red-heart.png"
         },
         river1: {
-            title: "First Change",
-            content: "When we decided...",
+            title: "Big Change",
+            content: "The way we went from short text to calling and seeing each other always makes me happy",
             heart: "red-heart.png"
         },
         river2: {
             title: "Growing Phase",
-            content: "The way we...",
+            content: "We have many differences but we always manage to work around them and adapt to each other",
             heart: "red-heart.png"
         },
         river3: {
             title: "Current Transition",
-            content: "As we...",
+            content: "I'm working on my sleep and time schudule in general, i want to be have as much time as possible with my beloved",
             heart: "red-heart.png"
         },
         star1: {
             title: "Dream 1",
-            content: "I can't wait...",
+            content: "The first intimate dream i had about us wasn't that crazy but it's still very special, i remember how my heart was racing",
             heart: "pink-heart.png"
         },
         star2: {
             title: "Dream 2",
-            content: "One day...",
+            content: "I keep remembering the Hu Tao one randomly, oh and i won't forget about you wearing her costume for me one day",
             heart: "pink-heart.png"
         },
         star3: {
             title: "Dream 3",
-            content: "I envision...",
+            content: "I love our hug and cuddle/ calm and cute dreams, nothing too wild, just us enjoying each other's company",
             heart: "pink-heart.png"
         },
         star4: {
             title: "Dream 4",
-            content: "Our plan...",
+            content: "THe dream where i basically see our future and how we get married is great too, insha'allah things turn out like that",
             heart: "pink-heart.png"
         },
         star5: {
             title: "Dream 5",
-            content: "Someday...",
+            content: "I love the teasing dreams too 🙃, not my fault you look so cute when we mess around",
             heart: "pink-heart.png"
         }
     };
